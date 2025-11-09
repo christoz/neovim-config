e248b531e8ff582b45619aef5500a4920da89eb7
Christos Ziogas <ziogasc@gmail.com>
2025-11-09 18:40:18 +0200

    from scratch

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,69 @@
+return {
+  {
+  'williamboman/mason.nvim',
+  config = function()
+    require('mason').setup()
+  end
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'lua_ls', 'ts_ls' }
+      }) 
+    end
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      -- Configure diagnostic display
+      vim.diagnostic.config({
+        virtual_text = true,  -- Show error text inline
+        signs = true,         -- Show signs in the sign column
+        underline = true,     -- Underline errors
+        update_in_insert = false,  -- Don't update diagnostics while typing
+        severity_sort = true, -- Sort by severity
+      })
+
+      -- Customize diagnostic signs
+      local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
+
+      -- Create autocmd to start LSP servers for specific filetypes
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'lua' },
+        callback = function()
+          vim.lsp.start({
+            name = 'lua_ls',
+            cmd = { 'lua-language-server' },
+            root_dir = vim.fs.root(0, { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git' }),
+          })
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+        callback = function()
+          vim.lsp.start({
+            name = 'ts_ls',
+            cmd = { 'typescript-language-server', '--stdio' },
+            root_dir = vim.fs.root(0, { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' }),
+          })
+        end,
+      })
+
+      -- Set keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {})
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {})
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {})
+    end
+  }
+}
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require('neo-tree').setup({
+      event_handlers = {
+        {
+          event = 'file_opened',
+          handler = function(file_path)
+            -- auto close
+            require('neo-tree.command').execute({ action = 'close' })
+          end
+        },
+      }
+    })
+
+    -- Keymap for Neotree
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle reveal left <CR>')
+  end
+}
